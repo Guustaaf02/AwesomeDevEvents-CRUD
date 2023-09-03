e8c75bddbdb9c33d96bdcf1376f668d9d5555dc6
Guustaaf02 <105667526+Guustaaf02@users.noreply.github.com>
2023-09-03 16:48:09 -0300

    Primeiro Commit

---
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,12 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTtitle { get; set; }
+        public string TalkTDescription { get; set; }
+
+        public string LinkedInProfile { get; set; }
+    }
+}
